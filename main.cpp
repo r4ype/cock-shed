fd9b35604479d419a2abc35782136593097b7c10
parsa <raypemaster@gmail.com>
2025-01-24 21:55:57 +0330

    basic device and drink class

--- main.cpp
+++ main.cpp
@@ -1,36 +1,67 @@
 #include "button.hpp"
 #include <SDL2/SDL.h>
 #include <iostream>
-
+#include <fstream>
 
 
 class Drink {
 public:
   int price;
-  int stock;
+  int count;
   int id;
   
 };
 
 
 class Device {
+private:
+  Drink stock[4];
+  
 public:
-  int showSotck();
-  int transaction();
+  void loadStock();
+  void recharge();
+  void updateStock();
+  void showSotck();
+  void transaction();
+  void updateTransaction();
+
+};
+
+void Device::loadStock(){
+std::ifstream inputFile("stockfile.dat");
+
+    if (inputFile.is_open()) {
+        std::string line;
+        while (std::getline(inputFile, line)) {
+            std::cout << line << std::endl;
+        }
+        inputFile.close();
+    } else {
+        std::cerr << "Unable to open file" << std::endl;
+    }
+
 }
 
-
-
-
-
 void lemonade() {
-    std::cout << " [1] sharbat limo kharide shod" << std::endl;
+ std::cout << "clicked"; 
 }
 
 int main() {
     SDL_Init(SDL_INIT_VIDEO);
-    SDL_Window* window = SDL_CreateWindow("cock-shed", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 800, 600, 0);
+    SDL_Window* window = SDL_CreateWindow("cock-shed", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 400, 600, 0);
     SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
+    
+    Device vendingMachine;
+    vendingMachine.loadStock();
+    Drink cocacola;
+    cocacola.price = 10;
+    cocacola.count = 0;
+    cocacola.id = 1;
+
+    Drink alis;
+    cocacola.price = 5;
+    cocacola.count = 0;
+    cocacola.id = 2;
 
     SDL_Color red = {255, 0, 0, 255};
     Button lemonadeB(100, 100, 200, 50, red);
